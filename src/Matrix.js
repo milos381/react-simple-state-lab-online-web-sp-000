5c403e55ae3d963dfe62bc2f25ed11e72c7f5486
milos381 <milos_popovic@outlook.com>
2021-03-22 12:40:11 -0400

    commit tree 419d0ce3b2e5616addb786bb4fe49c4a285151f5

--- src/Matrix.js
+++ src/Matrix.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
-
+import Cell from './Cell.js'
 export default class Matrix extends Component {
   
   genRow = (vals) => {
-    return vals.map(val => <div className="cell"></div>) // replace me and render a cell component instead!
+    return vals.map(val => <Cell value={val} />) // rendering a cell component!
   }
   
   genMatrix = () => {
+    
     return this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
+    //creating new ROWS that have a className="row"
+    //while invoking a function this.genRow(rowVals) that is creating cells
   }
   
   render() {
@@ -18,4 +21,19 @@ export default class Matrix extends Component {
     )
   }
   
+}
+Matrix.defaultProps = {
+  values: [
+  ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+  ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+  ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+  ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+  ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+  ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+  ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+  ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+  ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00'],
+  ['#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00', '#F00']
+] 
+
 }
